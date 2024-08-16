375ba6ae610c0c954c5d60a481a8f562190f5fb9
Hakansabol <hakansabol@gmail.com>
2024-08-16 11:21:26 -0700

    NOT and LSHIFT
    Errors fixed, now NOT and LSHIFT are available in the bitstring class

--- src/bst_main.java
+++ src/bst_main.java
@@ -1,39 +1,72 @@
 // Helper Class: Bitstring
 class bitstring {
     public bitstring(String nv) {
-        bits = new boolean[0];
+        boolean[] nb = new boolean[nv.length()] ;
+        int i = 0;
         for (char c : nv.toCharArray()) {
             if (c == '0') {
-                bits.add(false);
+                nb[i] = false;
             }
             else if (c == '1') {
-                bits.add(true);
+                nb[i] = true;
             }
+            i ++;
         }
+        bits = nb;
     }
-    private boolean[] bits;
+    private boolean[] bits = { };
 
-    public string Value() {
+    public String Value() {
         String ns = "";
         for (boolean b : bits) {
             if (b) {
-                ns.concat("1");
+                ns = ns.concat("1");
             }
-            else if (!b) {
-                ns.concat("0");
+            else {
+                ns = ns.concat("0");
             }
         }
+        return ns;
+    }
+
+    public void print() {
+        System.out.println(Value());
+    }
+
+    public String NOT() {
+        boolean[] nb = new boolean[bits.length];
+        for (int i = 0; i < nb.length; i ++) {
+            nb[i] = !bits[i];
+        }
+        bits = nb;
+        return Value();
+    }
+
+    public String LSHIFT(int mod) {
+        boolean[] nb = new boolean[bits.length];
+        for (int i = 0; i < nb.length; i ++) {
+            if (i + mod < bits.length) nb[i] = bits[i + mod];
+            else nb[i] = false;
+        }
+        bits = nb;
+        return Value();
     }
 }
 
 // Helper Class: Solver
 class bst_utils {
-    public string
 }
 
 // Execution Class
 class bst_main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        bitstring btst = new bitstring("100010");
+        btst.print();
+        btst.NOT();
+        btst.print();
+        btst.LSHIFT(1);
+        btst.print();
     }
 }
