aa7c736a44f78728faeecd932b1277190f57edaf
Hakansabol <hakansabol@gmail.com>
2024-09-18 22:58:29 -0700

    Testing
    I really don't understand parseInt and valueOf, but we're all here to learn, right?

--- src/computer_numbering_systems.java
+++ src/computer_numbering_systems.java
@@ -2,18 +2,22 @@ import java.util.Random;
 
 public class computer_numbering_systems {
     public static void main(String[] args) {
-        int rnum = 3564;
+        int rnum = 5;//(int)(Math.random() * 150000);
+        int cb = 2;
         String snum = Integer.toString(rnum);
 
         System.out.println(snum);
-        System.out.println(ctInt(snum, 16));
+        System.out.println(ctInt(snum, cb));
+        System.out.println(ctBase(snum, cb));
 
     }
 
+    // Turns num FROM base ten to specified base
     public static String ctInt(String str, int base) {
         return Integer.toString(Integer.parseInt(str, base));
     }
 
+    // Turns num FROM specified base to base 10
     public static String ctBase(String num, int base) {
         return Integer.toString(Integer.valueOf(num, base));
     }
